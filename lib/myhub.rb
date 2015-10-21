2bfd3ad4e7981528c51cec0f1356b306e7b0bfd7
Carl Westmoreland <beguira86@gmail.com>
2015-10-21 17:26:26 -0400

    First attempt, anger rising...

--- lib/myhub.rb
+++ lib/myhub.rb
@@ -1,6 +1,7 @@
 require "sinatra/base"
 require "httparty"
 require "pry"
+require "json"
 
 require "myhub/version"
 require "myhub/github"
@@ -12,6 +13,18 @@ module Myhub
     # Your code here ...
     get "/" do
       api = Github.new
+      data = api.list_issues
+      stuff = data.map do |x|
+        {
+              title: x["title"],
+              url: x["url"],
+              id: x["id"],
+              state: x["state"],
+              created: x["created"],
+              updated: x["updated"]
+        }
+        end 
+      binding.pry
       # get stuff from github
       erb :index, locals: { issues: stuff }
     end
@@ -25,6 +38,7 @@ module Myhub
     post "/issue/close/:id" do
       api = Github.new
       api.close_issue(params["id"].to_i)
+
       "Cool cool cool"
     end
 
--- lib/myhub/github.rb
+++ lib/myhub/github.rb
@@ -10,5 +10,27 @@ module Myhub
         "User-Agent"     => "HTTParty"
       }
     end
+
+    def list_issues
+      Github.get("/orgs/TIY-ATL-ROR-2015-Sep/issues",:headers => @headers, query: { state: "all"})
+    end  
+
+    def reopen_issue
+      Github.post("/repos/beguira86/assignments/issues/:number",:headers => @headers, :body => { state: 'closed'}.to_json)
+    end
+
+    def close_issue
+      Github.post("repos/beguira86/assignments/issues/:number",:headers => @headers, :body => { state: 'open'}.to_json)
+    end
+
+
+
+
+
+
+
+
+
+
   end
 end
